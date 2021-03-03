ad960da50afeee681336085fa319be04c8f5ca17
Ailie McCorkindale <ailiem134@hotmail.com>
2021-03-03 23:34:08 +0100

    rainbow particle effect

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let particlesArray = [];
+let hue = 0;
 
 window.addEventListener("resize", function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    ctx.fillStyle = "white";
-    ctx.fillRect(10, 10, 50, 50);
+    //ctx.fillStyle = "white";
+    //ctx.fillRect(10, 10, 50, 50);
 });
 
 const mouse = {
@@ -16,28 +17,37 @@ const mouse = {
     y: undefined
 }
 
+canvas.addEventListener("click", function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for(let i = 0; i < 10; i++) {
+        particlesArray.push(new Particle());
+    }
+    //Math.random() >=0.5 ? drawCircle() : drawRectangle();
+    
+})
 
-function drawCircle() {
-    ctx.fillStyle = "pink";
-    ctx.beginPath();
-    ctx.arc(mouse.x, mouse.y, 20, 0, Math.PI*2);
-    ctx.fill();
-}
+canvas.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+   // drawCircle();
+   // console.log(mouse);
+    for(let i = 0; i < 5; i++) {
+        particlesArray.push(new Particle());
+    }
+})
 
-function drawRectangle() {
-    ctx.fillStyle = "purple";
-    ctx.fillRect(mouse.x, mouse.y, 50, 50)
-}
 
 class Particle {
     constructor(){
-        //this.x = mouse.x;
-        //this.y = mouse.y;
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = mouse.x;
+        this.y = mouse.y;
+        //this.x = Math.random() * canvas.width;
+        //this.y = Math.random() * canvas.height;
         this.size = Math.random() * 15 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
+        this.color = 'hsl(' + hue +', 100%, 50%)';
     }
 
     update() {
@@ -49,7 +59,8 @@ class Particle {
 
     draw() {
         //ctx.fillStyle = Math.random() >= 0.5 ? "green" : "purple";
-        ctx.fillStyle = Math.random() >= 0.3 ? "yellow" : "red";
+        //ctx.fillStyle = Math.random() >= 0.3 ? "yellow" : "red";
+        ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
         ctx.fill();
@@ -57,42 +68,36 @@ class Particle {
 }
 
 
-canvas.addEventListener("click", function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    particlesArray.push(new Particle());
-    //Math.random() >=0.5 ? drawCircle() : drawRectangle();
-    
-})
-
-canvas.addEventListener('mousemove', function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-   // drawCircle();
-    console.log(mouse);
-})
-
-
-function initParticles() {
-    for(let i = 0; i < 100; i++) {
-        particlesArray.push(new Particle());
-    }
-}
-initParticles();
-console.log(particlesArray)
-
 function handleParticles() {
     for(let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
         particlesArray[i].draw();
+        if(particlesArray[i].size <= 0.3) {
+            particlesArray.splice(i,1);
+            i--;
+        }
     }
 }
-
+/*
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    requestAnimationFrame(animate);
+   // ctx.fillStyle = 'rbga(0,0,0,0.05)';
+   // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //console.log(ctx.fillStyle)
     //drawCircle();
     handleParticles();
+    hue++;
+    requestAnimationFrame(animate);
+}*/
+
+function animate(){
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(0,0,0,0.02)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    hue++
+    //frame++;
+    requestAnimationFrame(animate);
 }
 
 animate();
--- test.js
+++ test.js
@@ -11,4 +11,25 @@ ctx.beginPath();
 ctx.arc(300, 100, 50, 0, Math.PI*2);
 ctx.stroke();
 ctx.fill();
+
+
+function initParticles() {
+    for(let i = 0; i < 100; i++) {
+        particlesArray.push(new Particle());
+    }
+}
+initParticles();
+console.log(particlesArray)
+
+function drawCircle() {
+    ctx.fillStyle = "pink";
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 20, 0, Math.PI*2);
+    ctx.fill();
+}
+
+function drawRectangle() {
+    ctx.fillStyle = "purple";
+    ctx.fillRect(mouse.x, mouse.y, 50, 50)
+}
 */
